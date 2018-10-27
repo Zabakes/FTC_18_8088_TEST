6bef501269c664fbe1c6bed595552667c7301dff
Unknown <zeusay170@gmail.com>
2018-10-26 21:13:42 -0400

    New swerve math and encoder thread

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderThread.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderThread.java
@@ -2,143 +2,47 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-/**
- * Runs a DC motor to a given position in it's own thread other things can happen while the motor is running to that position
- */
-public class EncoderThread implements Runnable {
+public class EncoderThread extends Thread {
 
     DcMotor motor;
 
-    private double targetPosition;
-    private double power;
-    private double TICKS_PER_REV;
-    private double radius = 0;
-    Thread t = new Thread(this);
+    int position;
+    int power;
 
-    /**
-     * @param motor this is the motor that the encoder is attached to
-     * @param gearReduction this is is the gear reduction in that motor if a neverest 40 then this is 40
-     */
-    public EncoderThread(DcMotor motor, double gearReduction) {
+    public EncoderThread(DcMotor motor){
         this.motor = motor;
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        TICKS_PER_REV = 7 * gearReduction;
     }
 
-    /**
-     * runs to a set position at a set speed with a position set by setTargetPosition and speed set by setPower
-     *
-     * or called by another method in this class
-     */
     @Override
-    public void run() {
+    public void run(){
 
-        //record all the initial stuff
         long t = System.currentTimeMillis();
-        double initialPos = motor.getCurrentPosition();
-        DcMotor.RunMode mode = motor.getMode();
 
-        //run to the position
+        double initalpos = motor.getCurrentPosition();
+
+        DcMotor.RunMode mode = motor.getMode();
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        motor.setTargetPosition((int) Math.round(targetPosition*(power/Math.abs(power))));
+
+        motor.setTargetPosition(position);
         motor.setPower(power);
 
-        //wait to get to the position or timeout
-        while (motor.isBusy() && System.currentTimeMillis() < t + Math.abs(targetPosition - initialPos) * power && Mech.opModeIsactive) {
+        while(motor.isBusy() && System.currentTimeMillis() < t+Math.abs(position - initalpos)*1*power){
 
         }
 
-        //turn off and set the mode back to the original
         motor.setPower(0);
         motor.setMode(mode);
 
+        }
+
+    public void setPosition(int position) {
+        this.position = position;
     }
 
-    /**
-     * sets the target position in ticks for the motor belonging to this object DOES NOT run to that position
-     *
-     * @param targetPosition target position in ticks
-     */
-    public void setTargetPosition(int targetPosition) {
-        this.targetPosition = targetPosition;
-    }
 
-    /**
-     * sets the power for this object DOES NOT run to that position
-     *
-     * @param power power to set
-     */
     public void setPower(int power) {
         this.power = power;
     }
 
-    /**
-     *
-     * runs to a position in ticks in it's own thread
-     *
-     * @param power power to run at
-     * @param targetposition position to run to
-     */
-    public void runToPosition(int power, int targetposition) {
-
-        //run to a  position
-        this.power = power;
-        this.targetPosition = targetposition;
-        if(!t.isAlive()) {//make sure only one thread at a time is using this motor
-            t.start();
-        }
-    }
-
-
-    /**
-     *
-     * runs to a linear position using encoders and a given radius in it's own thread
-     *
-     * @param power sets the power to run the motor at
-     * @param targetPosition sets a linear position for the motor to run to
-     * @param radius radius of the wheel or pulley attached to the wheel
-     */
-    @Deprecated
-    public void runToPosLinear(double power, double targetPosition, double radius) {
-
-        //run to a linear pos based on a new radius
-        this.power = power;
-        this.targetPosition = targetPosition * 2 * Math.PI * radius;
-        if(!t.isAlive()) {//make sure only one thread at a time is using this motor
-            t.start();
-        }
-    }
-    /**
-     *
-     * runs to a linear position using encoders with a radius already set and stored with the other runToPosLinear or setRadius in it's own thread
-     *
-     * @param power sets the power to run a motor at
-     * @param targetPosition sets a linear position for the motor to run to
-     */
-    public void runToPosLinear(double power, double targetPosition) {
-
-        //run to a linear position based on a set radius
-        this.power = power;
-        this.targetPosition = (targetPosition)/((2 * Math.PI * radius)/TICKS_PER_REV);
-        if(!t.isAlive()) {//make sure only one thread at a time is using this motor
-            t.start();
-        }
-    }
-
-    /**
-     * @return linear position of the motor based on the set radius
-     */
-    public double getLinearPos() {
-        //get a linear pos based on a radius
-        return (motor.getCurrentPosition())/((2 * Math.PI * radius)/TICKS_PER_REV);
-    }
-
-    /**
-     * sets the radius of a wheel or pulley attached to a motor for running to positions linearly
-     *
-     * @param radius radius to set to
-     */
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
 }
