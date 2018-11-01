e1010b3a1a96eac65e58229cb361d067d4c153eb
Unknown <zeusay170@gmail.com>
2018-11-01 18:41:49 -0400

    made multi-threading optional when using mech and added methods for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mech.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mech.java
@@ -3,18 +3,27 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-abstract class MechThread implements Runnable{
+abstract class Mech implements Runnable{
 
+    public void setThread(boolean thread) {
+        isThread = thread;
+    }
+
+    protected boolean isThread = false;
     Gamepad gamepad;
-    Thread tempThread = null;
+    Thread thread = null;
 
     public abstract void init(HardwareMap hardwareMap);
 
     public void updateAndStart(Gamepad gamepad){
         this.gamepad = gamepad;
-        if(!tempThread.isAlive() || tempThread == null) {
-            tempThread = new Thread(this);
-            tempThread.start();
+        if(isThread) {
+            if (!thread.isAlive() || thread == null) {
+                thread = new Thread(this);
+                thread.start();
+            }
+        }else{
+            this.run();
         }
     }
 
