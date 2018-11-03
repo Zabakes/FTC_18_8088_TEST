1d416780b090dea098a8bb50a48ea8e966bb923c
Unknown <zeusay170@gmail.com>
2018-11-03 17:53:32 -0400

    added comments and a javadoc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderThread.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderThread.java
@@ -2,7 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class EncoderThread implements Runnable{
+/**
+ * Runs a DC motor to a given position in it's own thread other things can happen while the motor is running to that position
+ */
+public class EncoderThread implements Runnable {
 
     DcMotor motor;
 
@@ -11,68 +14,126 @@ public class EncoderThread implements Runnable{
     private double TICKS_PER_REV;
     private double radius;
 
-    public EncoderThread(DcMotor motor, double gearReduction){
+    /**
+     * @param motor this is the motor that the encoder is attached to
+     * @param gearReduction this is is the gear reduction in that motor if a neverest 40 then this is 40
+     */
+    public EncoderThread(DcMotor motor, double gearReduction) {
         this.motor = motor;
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        TICKS_PER_REV = 7*gearReduction;
+        TICKS_PER_REV = 7 * gearReduction;
     }
 
+    /**
+     * runs to a set position at a set speed with a position set by setTargetPosition and speed set by setPower
+     *
+     * or called by another method in this class
+     */
     @Override
-    public void run(){
+    public void run() {
 
+        //record all the initial stuff
         long t = System.currentTimeMillis();
-
-        double initalpos = motor.getCurrentPosition();
-
+        double initialPos = motor.getCurrentPosition();
         DcMotor.RunMode mode = motor.getMode();
-        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+        //run to the position
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motor.setTargetPosition((int) Math.round(targetPosition));
         motor.setPower(power);
 
-        while(motor.isBusy() && System.currentTimeMillis() < t+Math.abs(targetPosition - initalpos)*1*power){
+        //wait to get to the position or timeout
+        while (motor.isBusy() && System.currentTimeMillis() < t + Math.abs(targetPosition - initialPos) * 1 * power) {
 
         }
 
+        //turn off and set the mode back to the original
         motor.setPower(0);
         motor.setMode(mode);
 
-        }
+    }
 
-    public void setTargetposition(int targetPosition) {
+    /**
+     * sets the target position in ticks for the motor belonging to this object DOES NOT run to that position
+     *
+     * @param targetPosition target position in ticks
+     */
+    public void setTargetPosition(int targetPosition) {
         this.targetPosition = targetPosition;
     }
 
-
+    /**
+     * sets the power for this object DOES NOT run to that position
+     *
+     * @param power power to set
+     */
     public void setPower(int power) {
         this.power = power;
     }
 
-    public void runToPosition(int power, int targetposition){
+    /**
+     *
+     * runs to a position in ticks in it's own thread
+     *
+     * @param power power to run at
+     * @param targetposition position to run to
+     */
+    public void runToPosition(int power, int targetposition) {
+
+        //run to a  position
         this.power = power;
         this.targetPosition = targetposition;
         Thread t = new Thread(this);
         t.start();
+
     }
 
-    public void runToPosLinear(double power, double targetPosition, double radius){
+
+    /**
+     *
+     * runs to a linear position using encoders and a given radius in it's own thread
+     *
+     * @param power sets the power to run the motor at
+     * @param targetPosition sets a linear position for the motor to run to
+     * @param radius radius of the wheel or pulley attached to the wheel
+     */
+    public void runToPosLinear(double power, double targetPosition, double radius) {
+
+        //run to a linear pos based on a new radius
         this.power = power;
-        this.targetPosition = targetPosition*2*Math.PI*radius;
+        this.targetPosition = targetPosition * 2 * Math.PI * radius;
+        Thread t = new Thread(this);
+        t.start();
+    }
+    /**
+     *
+     * runs to a linear position using encoders with a radius already set and stored with the other runToPosLinear or setRadius in it's own thread
+     *
+     * @param power sets the power to run a motor at
+     * @param targetPosition sets a linear position for the motor to run to
+     */
+    public void runToPosLinear(double power, double targetPosition) {
+
+        //run to a linear position based on a set radius
+        this.power = power;
+        this.targetPosition = (targetPosition / TICKS_PER_REV) * (2 * Math.PI * radius);
         Thread t = new Thread(this);
         t.start();
     }
 
-    public void runToPosLinear(double power, double targetPosition){
-        this.power = power;
-        this.targetPosition = (targetPosition/TICKS_PER_REV)*(2*Math.PI*radius);
-        Thread t = new Thread(this);
-        t.start();
+    /**
+     * @return linear position of the motor based on the set radius
+     */
+    public double getLinearPos() {
+        //get a linear pos based on a radius
+        return (motor.getCurrentPosition() / TICKS_PER_REV) * (2 * Math.PI * radius);
     }
 
-    public double getLinearPos(){
-        return (motor.getCurrentPosition()/TICKS_PER_REV)*(2*Math.PI*radius);
-    }
-
+    /**
+     * sets the radius of a wheel or pulley attached to a motor for running to positions linearly
+     *
+     * @param radius radius to set to
+     */
     public void setRadius(double radius) {
         this.radius = radius;
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mech.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mech.java
@@ -3,27 +3,54 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-abstract class Mech implements Runnable{
+/**
+ *an abstract definition of a mechanism including the ability to run on their own threads
+ *
+ *all mechs must implement run and init
+ *
+ *run is called by update and start which should be called in telop
+ *
+ *init should be called on init in both teleop and auto
+ *
+*/
 
-    public void setThread(boolean thread) {
+public abstract class Mech implements Runnable {
+
+    /**
+     * set whether or not a given mech should run on it's own thread
+     *
+     * @param thread true if mech should run on it's own thread false if not
+     */
+    public void setThread(boolean thread) {//ability to turn threads on/off
         isThread = thread;
     }
 
-    protected boolean isThread = false;
-    Gamepad gamepad;
-    Thread thread = null;
+    protected boolean isThread = false;//turn threads off by default
+    Gamepad gamepad;//gamepad used for running the mechs
+    Thread thread = new Thread(this);//the thread with this runnable (this run method)
 
+    /**
+     *
+     * initialize a given mechanism this is because then mechanisms are completely independent
+     *
+     * @param hardwareMap hardwaremap from the phone
+     */
     public abstract void init(HardwareMap hardwareMap);
 
-    public void updateAndStart(Gamepad gamepad){
-        this.gamepad = gamepad;
-        if(isThread) {
-            if (!thread.isAlive() || thread == null) {
-                thread = new Thread(this);
-                thread.start();
+    /**
+     *
+     * run the teleop functions of a mechanism on or off it's own thread this keeps mechanisms isolated from opmodes so it's easy to take something off the robot. calls the run method of a mechanism if it runs on it's own thread this will not run two threads for a single mechanism
+     *
+     * @param gamepad gamepad from opmode to run based on
+     */
+    public void updateAndStart(Gamepad gamepad) {// update and start method for all mechs updates the gamepad and starts run
+        this.gamepad = gamepad;//update gamepad
+        if (isThread){//if this mech uses it's own thread.
+            if (!thread.isAlive()) {//check to make sure no thread of this mech is running already.
+                thread.start();//start the thread with this mech is a runnable.
             }
-        }else{
-            this.run();
+        } else {
+            this.run();//otherwise just call the run method normally
         }
     }
 
