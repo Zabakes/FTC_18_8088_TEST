5b8cb8c6100b59434b80e59aa0dc75aae2932bc5
Unknown <zeusay170@gmail.com>
2019-11-25 12:10:14 -0500

    Deleted last year's crap

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumChassis.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MechanumChassis extends Mech {
+    @Override
+    public void init(HardwareMap hardwareMap) {
+        frontLeft = 
+    }
+
+    @Override
+    public String name() {
+        return null;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
